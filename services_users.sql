50a4c19774759f9f2a444ac225feb10314cd223b
Achour Riyad <riyad.achour@edu.itescia.fr>
2018-10-22 16:36:42 +0200

    Auto stash before merge of "master" and "origin/master"

--- services_users.sql
+++ services_users.sql
@@ -1,15 +1,8 @@
 CREATE TABLE services_users(
 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-<<<<<<< Updated upstream
-=======
 id_service INT UNSIGNED NOT NULL,
->>>>>>> Stashed changes
 id_user INT UNSIGNED NOT NULL,
 sign_date_hour DATETIME DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY(id)
 )
-<<<<<<< Updated upstream
 ENGINE = INNODB;
-=======
-ENGINE = INNODB;
->>>>>>> Stashed changes

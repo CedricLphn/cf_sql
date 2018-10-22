b8c8bc3d8decf71ee0bd908ce84194eee8153b0b
Achour Riyad <riyad.achour@edu.itescia.fr>
2018-10-22 17:07:04 +0200

    créations services_users

--- services_users.sql
+++ services_users.sql
@@ -6,3 +6,16 @@ sign_date_hour DATETIME DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY(id)
 )
 ENGINE = INNODB;
+
+INSERT INTO services_users (id_user, id_service, sign_date_hour)
+VALUES
+	(2 , 5 , 2018-01-08),
+	(13 , 12 , 2018-02-15),
+	(24 , 19 , 2018-03-06),
+	(15 , 11 , 2018-04-01),
+	(1 , 15 , 2018-05-22),
+	(4 , 13 , 2018-06-04),
+	(6 , 2 , 2018-07-12),
+	(9 , 9 , 2018-08-19),
+	(19 , 6 , 2018-09-13),
+	(16 , 1 , 2018-10-27),
